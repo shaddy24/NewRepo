68f2597936040d9c80338ef08085c315bd54a0f0
balanday.shaddy@gmail.com <balanday.shaddy@gmail.com>
2021-01-27 17:15:14 +0800

    tryythus

---
+++ Merger/Merger/tryy1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Merger
+{
+    class tryy1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
